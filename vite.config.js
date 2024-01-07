08909825264f492b9423a476fb6ecae2c587a961
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2024-01-07 00:30:39 -0800

    icons

--- vite.config.js
+++ vite.config.js
@@ -21,12 +21,12 @@ export default {
         theme_color: '#000000',
         icons: [
           {
-            src: '/public/android-chrome-192x192.png', // Path to your icons
+            src: '/android-chrome-192x192.png', // Path to your icons
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/public/android-chrome-512x512.png',
+            src: '/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
